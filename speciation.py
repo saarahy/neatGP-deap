6b69b3587bd2c136329e2529f7249099ef20d726
saarahy <juarez.s.perla@gmail.com>
2015-08-24 10:23:35 -0700

    count specie

--- speciation.py
+++ speciation.py
@@ -4,17 +4,21 @@ def init_species(population):
     for ind in population:
         ind.specie(1)
 
-def count_species():
-    return 0
+def count_species(population):
+    contador=0
+    for ind in population:
+        if ind.get_specie!= None:
+            contador+=1
+    return contador
 
 def species(population, h):
     for ind in population:
-        if ind.specie==None:
+        if ind.get_specie==None:
             for ind1 in population:
                 if distance(ind,ind1)<=h:
                     ind.specie=ind1.get_specie()
                     break
-            if ind.specie==None:
+            if ind.get_specie==None:
                 num_specie=count_species()
                 ind.specie=num_specie+1
 
