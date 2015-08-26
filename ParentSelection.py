876944c9bb64ec6adc04019ff87db328bcf833c4
saarahy <juarez.s.perla@gmail.com>
2015-08-26 10:05:52 -0700

    Parent Selection algorithm 1

---
+++ ParentSelection.py
@@ -0,0 +1,17 @@
+from deap import gp
+
+class p_selection():
+    def p_selection(self, population):
+        q=list()
+        for ind in population:
+            ind.descendent()
+        q=population
+        for ind in q:
+            print 'eliminar individuos'
+    #ordenar individuos basados en penalizacion
+
+    def num_desc(self, ind, avg):
+        print 'num de descendientes de cada individuo'
+
+    def avg(self, population):
+        print 'promedio de fitness de la poblacion'
