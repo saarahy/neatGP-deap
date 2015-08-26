c9b14493b0e137fad6019dc58d2ef9cfe00a34cc
saarahy <juarez.s.perla@gmail.com>
2015-08-26 10:09:06 -0700

    Parent Selection algorithm 1

--- ParentSelection.py
+++ ParentSelection.py
@@ -4,12 +4,11 @@ class p_selection():
     def p_selection(self, population):
         q=list()
         for ind in population:
-            ind.descendent()
+            ind.descendent(num_desc(ind, avg(population)))
         q=population
         for ind in q:
             print 'eliminar individuos'
     #ordenar individuos basados en penalizacion
-
     def num_desc(self, ind, avg):
         print 'num de descendientes de cada individuo'
 
