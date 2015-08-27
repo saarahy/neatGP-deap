7984802166e6f13df2f971de9264eeb6536a71de
saarahy <juarez.s.perla@gmail.com>
2015-08-27 14:01:39 -0700

    ParentSelection complete

--- ParentSelection.py
+++ ParentSelection.py
@@ -1,8 +1,8 @@
 from deap import gp
 from speciation import *
-class p_selection():
-    #debe de recibir de entrada el parametro de
+#debe de recibir de entrada el parametro de
     #survival_thershold=0.5
+class p_selection():
     def p_selection(self, population):
         #provisional
         survival=0.5
@@ -11,40 +11,39 @@ class p_selection():
         #penalizar al promedio
         if prom_ap_penal>10:
             prom_ap_penal=10
-        q=list()
+
+
         #conseguimos y aplicamos el num. de desc.
         # a cada individuo
         for ind in population:
             ind.descendent(num_desc(ind, prom_ap_penal))
 
-        numbest=round(len(population)*survival)
         q=population
 
-        #ordenar poblacion por su fitness
-        allpotfit=sort_fitness(q)
         #obtener numero de especies
-        specie=ind_specie(population)
+        specie=ind_specie(q)
+        #crear grupos auxilares
+        gpo_specie=list()
+        parents=list()
+
         for e in specie:
-            contador=0
-            for ind in population:
+            for ind in q:
                 if ind.get_specie()==e[0]:
-                    contador+=1
-                    if round(ind.get_numspecie() * survival)<contador:
-                        ind.fitness.values=9999999
-                        if ind.get_numspecie==contador:
-                            break
+                    gpo_specie.append(ind)
+            parents.append(eliminar_ind(gpo_specie,survival))
+
 
 
 
 def num_desc(self, ind, avg):
-    numd=round(avg/ind.get_fsharing())
+    numd=round(avg/ind.fitness.values)
     return numd
 
 
 def avg(self, population):
     suma=0
     for ind in population:
-        suma+=ind.get_fsharing()
+        suma+=ind.fitness.values
     promedio=suma/len(population)
     return promedio
 
@@ -53,5 +52,14 @@ def sort_fitness(population):
     orderbyfit=list()
     for ind in population:
         allpotfit.append([ind, ind.fitness.values, ind.get_fsharing])
-    orderbyfit=sorted(allpotfit, key=lambda ind: ind[1])
+    orderbyfit=sorted(allpotfit, key=lambda ind: ind[2])
     return orderbyfit
+
+def eliminar_ind(gpo_specie, survival):
+    sort_gpo=sort_fitness(gpo_specie)
+    indice=int(round(len(sort_gpo)-(len(sort_gpo)*survival)))
+    reverse_gpo=sorted(sort_gpo, reverse=True)
+    for i in range(indice):
+        del reverse_gpo[0]
+    sort_gpo=sort_fitness(reverse_gpo)
+    return sort_gpo
