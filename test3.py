ff7873dd67b780c9f822849b1fee77c10bc820df
saarahy <juarez.s.perla@gmail.com>
2015-09-01 07:42:09 -0700

    cruce neat v1.

---
+++ test3.py
@@ -0,0 +1,28 @@
+from deap import base, creator, gp, tools
+import  operator
+from crosspoints import *
+
+pset = gp.PrimitiveSet("MAIN", 1)
+pset.addPrimitive(operator.add, 2)
+pset.addPrimitive(operator.sub, 2)
+pset.addPrimitive(operator.mul, 2)
+pset.renameArguments(ARG0='x')
+
+creator.create("Individual", gp.PrimitiveTree)
+
+toolbox = base.Toolbox()
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=4)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+
+expr = toolbox.individual()
+expr2 = toolbox.individual()
+nodes, edges, labels = gp.graph(expr)
+
+print 'expr:',expr
+print 'expr2:',expr2
+
+cxp=crosspoints(expr,expr2)
+print cxp
+#print distance(expr,expr2)
+
+print neatcx(expr, expr2)
