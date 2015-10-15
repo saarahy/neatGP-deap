c7788c67d732a7970b1b5259e2d5c6ef13765787
saarahy <juarez.s.perla@gmail.com>
2015-10-15 15:39:55 -0700

    modifying files

--- Keijzer6.py
+++ Keijzer6.py
@@ -11,63 +11,15 @@ from deap import tools
 from deap import gp
 from neat_operators import neatGP
 from ParentSelection import sort_fitnessvalues
-
-
-def safe_div(left, right):
-    try:
-        return left / right
-    except ZeroDivisionError:
-        return 0
-
-
-def mylog(x):
-    if x==0:
-        return 0
-    else:
-        return math.log10(abs(x))
-
-
-def mysqrt(x):
-    if x<=0:
-        return 0
-    else:
-        return math.sqrt(x)
-
-
-def mypower2(x):
-    y=math.pow(x,2)
-    if isinstance(y,complex) or math.isinf(y) or math.isnan(y):
-        return 0
-    else:
-        return y
-
-
-def mypower3(x):
-    y=math.pow(x,3)
-    if isinstance(y,complex) or math.isinf(y) or math.isnan(y):
-        return 0
-    else:
-        return y
-
-
-def negative(x):
-    return -x
-
-
-def undivide(x):
-    if x==0:
-        return 1.0
-    else:
-        return 1.0/x
-
+from my_operators import undivide, negative, mysqrt
 
 pset = gp.PrimitiveSet("MAIN", 1)
 pset.addPrimitive(operator.add, 2)
 pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(undivide, 1)  # Koza, Korns
-pset.addPrimitive(negative, 1)  # Koza, Korns
-pset.addPrimitive(mysqrt, 1)  # Koza, Korns
-pset.renameArguments(ARG0='x')  # Koza
+pset.addPrimitive(undivide, 1)
+pset.addPrimitive(negative, 1)
+pset.addPrimitive(mysqrt, 1)
+pset.renameArguments(ARG0='x')
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("FitnessTest", base.Fitness, weights=(-1.0,))
@@ -82,21 +34,21 @@ toolbox.register("compile", gp.compile, pset=pset)
 
 def evalSymbReg(individual, points):
     func = toolbox.compile(expr=individual)
-    sqerrors=[]
+    sqerrors = []
     for elem in range(len(points)):
         y=[0.0]
-        num_div=[1.0/x for x in points[:elem]]
+        num_div = [1.0/x for x in points[:elem]]
         for x in range(elem):
-            y[0]+=num_div[x]
+            y[0] += num_div[x]
         sqerrors.append((func(points[elem])-(y[0]))**2)
     return math.fsum(sqerrors) / len(points),
 
 
 def Keijzer6(n_corr):
-    with open("./data_corridas/Keijzer6/corrida%d/test_x.txt" %n_corr) as spambase:
+    with open("./data_corridas/Keijzer6/corrida%d/test_x.txt" % n_corr) as spambase:
         spamReader = csv.reader(spambase)
         spam = [float(row[0]) for row in spamReader]
-    with open("./data_corridas/Keijzer6/corrida%d/train_x.txt"%n_corr) as spamb:
+    with open("./data_corridas/Keijzer6/corrida%d/train_x.txt"% n_corr) as spamb:
         spamReader2 = csv.reader(spamb)
         spam2 = [float(row[0]) for row in spamReader2]
     toolbox.register("evaluate", evalSymbReg, points=spam2)
@@ -124,9 +76,9 @@ def main(n_corr,p):
     mstats.register("max", numpy.max)
     params = ['best_of_each_specie', 2, 'yes']
     neatcx = True
-    alg = True
+    neat = True
     pelit = 0.6
-    pop, log = algorithms.eaSimple(pop, toolbox, 0.7, 0.3, 100, alg, neatcx, 0.15, pelit, n_corr, p, params, stats=mstats, halloffame=hof, verbose=True)
+    pop, log = algorithms.eaSimple(pop, toolbox, 0.7, 0.3, 100, neat, neatcx, 0.15, pelit, n_corr, p, params, stats=mstats, halloffame=hof, verbose=True)
 
     outfile = open('popfinal_%d_%d.txt'%(p,n_corr), 'w')
 
