69b25ea9b6cf427e9338d1958ed55eb1dc6c2047
saarahy <juarez.s.perla@gmail.com>
2015-11-10 01:21:40 -0800

    funcionando

--- alg.py
+++ alg.py
@@ -5,7 +5,7 @@ from speciation import *
 from fitness_sharing import *
 from ParentSelection import *
 
-def eaSimple(population, toolbox, cxpb, mutpb, ngen, neat, neatcx, h,pelit,n_corr, problem, params,stats=None,
+def eaSimple(population, toolbox, cxpb, mutpb, ngen, alg,neat, h,pelit,n_corr, p, params,stats=None,
              halloffame=None, verbose=__debug__):
     """This algorithm reproduce the simplest evolutionary algorithm as
     presented in chapter 7 of [Back2000]_.
@@ -15,14 +15,9 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, neat, neatcx, h,pelit,n_cor
                     operators.
     :param cxpb: The probability of mating two individuals.
     :param mutpb: The probability of mutating an individual.
-    ## Modify by Perla Juarez
     :param ngen: The number of generation.
     :param neat: wheter or not to use neatGP
-    :param neatcx: wheter or not to use neat-crossover
     :param h: indicate the distance allowed between each specie
-    :param pelit: indicate the probability of being random-elitist
-    :param n_corr: indicate the number of run
-    :param problem: indicate the number of problem that you are running (menu)
     :param params:indicate the params for the fitness sharing, the diffetent
                     options are:
                     -DontPenalize(str): 'best_specie' or 'best_of_each_specie'
@@ -31,7 +26,6 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, neat, neatcx, h,pelit,n_cor
                         2.penalization fitness sharing
                         3.new penalization
                     -ShareFitness(str): 'yes' or 'no'
-    ##
     :param stats: A :class:`~deap.tools.Statistics` object that is updated
                   inplace, optional.
     :param halloffame: A :class:`~deap.tools.HallOfFame` object that will
@@ -70,20 +64,46 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, neat, neatcx, h,pelit,n_cor
     logbook = tools.Logbook()
     logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
 
-    if neat:  # assign species to each individual on the population
-        species(population, h)  # h is the parameter for the dissimilarity
+    # out=open('popinit.txt','a')
+    # for ind in population:
+    #     out.write('\n  %s %s %s %s %s; %s' %(0,len(ind), ind.height, ind.get_specie(),  ind.get_fsharing(), ind))
+    # out.close()
+
+    #asignar especies en cada individuo en la poblacion
+    if alg:
+        #realiza la especiacion de la poblacion, dado un parametro h
+        species(population,h)
+        #asigna a cada individuo de la poblacion, el numero de individuos
+        #dentro de su misma especie
         ind_specie(population)
+
+    # out=open('popinitspecie.txt','a')
+    # for ind in population:
+    #     out.write('\n%s;%s;%s;%s;%s;%s' %(0,len(ind), ind.height, ind.get_specie(),  ind.get_fsharing(), ind))
+    # out.close()
 
     # Evaluate the individuals with an invalid fitness
     invalid_ind = [ind for ind in population if not ind.fitness.valid]
     fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-    fitnesses_test = toolbox.map(toolbox.evaluate_test, invalid_ind)
+    fitnesses_test=toolbox.map(toolbox.evaluate_test, invalid_ind)
     for ind, fit, fit_test in zip(invalid_ind, fitnesses, fitnesses_test):
         ind.fitness.values = fit
         ind.fitness_test.values = fit_test
 
-    if neat:
-        SpeciesPunishment(population, params, h)
+    # out=open('popbefpen.txt','a')
+    # for ind in population:
+    #     out.write('\n  %s %s %s %s %s %s %s' %(0,len(ind), ind.height, ind.get_specie(), ind.fitness.values, ind.get_fsharing(), ind))
+    # out.close()
+
+    #modificar aptitud en base al fitness sharing y la penalizacion
+    #dependiendo del parametro
+    if alg:
+        SpeciesPunishment(population,params,h)
+
+    # out=open('popaftpen.txt','a')
+    # for ind in population:
+    #     out.write('\n%s;%s;%s;%s;%s;%s;%s' %(0,len(ind), ind.height, ind.get_specie(), ind.fitness.values, ind.get_fsharing(), ind))
+    # out.close()
 
     if halloffame is not None:
         halloffame.update(population)
@@ -95,26 +115,56 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, neat, neatcx, h,pelit,n_cor
 
     # Begin the generational process
     for gen in range(1, ngen+1):
-        if neat:
-            parents = p_selection(population, gen)
+        # out=open('offinit_%d.txt','a')
+        # for ind in population:
+        #     out.write('\n%s;%s;%s;%s;%s;%s;%s' %(gen,len(ind), ind.height, ind.get_specie(), ind.fitness.values, ind.get_fsharing(), ind))
+        # out.close()
+        # Select the next generation individuals
+        if alg:
+            parents=p_selection(population, gen)
         else:
             parents = toolbox.select(population, len(population))
 
+        # out=open('parents_%d.txt'%n_corr,'a')
+        # for ind in parents:
+        #     out.write('\n%s;%s;%s;%s;%s;%s;%s;%s' %(gen,len(ind), ind.height, ind.get_specie(), ind.get_descendents(),ind.fitness.values, ind.get_fsharing(), ind))
+        # out.close()
         # Vary the pool of individuals
-        # here will be evaluated the parents pool with
-        # the neat crossover algorithm
-        if neatcx:
-            n = len(parents)
-            mut = 1  # 1. Activate mutation - Otherwise. Deactivate mutaction
-            cx = 1  # 1. Activate crossover - Otherwise. Deactivate crossover
-            offspring = neatGP(toolbox, parents, cxpb, mutpb, n, mut, cx, pelit)
+        #here will be evaluated the parents pool with
+        #the neat crossover algorithm
+        if neat:
+            n=len(parents)
+            mut=1
+            cx=1
+            offspring=neatGP(toolbox,parents,cxpb,mutpb,n,mut,cx,pelit)
         else:
             offspring = varAnd(parents, toolbox, cxpb, mutpb)
 
-        if neat:  # assigne the specie to each offspring
-            specie_parents_child(parents, offspring, h)
-            offspring[:] = parents+offspring
+
+        # for ind in parents:
+        #     out.write('\n  %s %s %s %s %s %s' %(gen,len(ind), ind.height, ind.get_specie(),  ind.get_fsharing(), ind))
+
+
+        #asignar especies en cada individuo en la poblacion
+        if alg:
+            #realiza la especiacion de la poblacion, dado un parametro h
+            #species(offspring,h)
+            specie_parents_child(parents,offspring,h)
+            #asigna a cada individuo de la poblacion, el numero de individuos
+            #dentro de su misma especie
+            # out=open('offtneat_%d.txt'%n_corr,'a')
+            # for ind in offspring:
+            #     out.write('\n%s;%s;%s;%s;%s;%s' %(gen,len(ind), ind.height, ind.get_specie(),  ind.get_fsharing(), ind))
+            # out.close()
+            offspring[:]=parents+offspring
             ind_specie(offspring)
+
+
+
+        # out=open('aftspecie.txt','a')
+        # for ind in offspring:
+        #     out.write('\n  %s %s %s %s %s %s' %(gen,len(ind), ind.height, ind.get_specie(), ind.get_fsharing(), ind))
+        # out.close()
 
         for ind in offspring:
             del ind.fitness.values
@@ -127,16 +177,26 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, neat, neatcx, h,pelit,n_cor
             ind.fitness.values = fit
             ind.fitness_test.values = fit_test
 
-        if neat:
-            SpeciesPunishment(offspring, params, h)
+        # out=open('offbefpen.txt','a')
+        # for ind in offspring:
+        #     out.write('\n  %s %s %s %s %s %s %s' %(gen,len(ind), ind.height, ind.get_specie(), ind.fitness.values, ind.get_fsharing(), ind))
+        # out.close()
+
+        if alg:
+            SpeciesPunishment(offspring,params,h)
+
+        # out=open('offaftpen.txt','a')
+        # for ind in offspring:
+        #     out.write('\n%s;%s;%s;%s;%s;%s;%s' %(gen,len(ind), ind.height, ind.get_specie(), ind.fitness.values, ind.get_fsharing(), ind))
+        # out.close()
 
         # Update the hall of fame with the generated individuals
         if halloffame is not None:
             halloffame.update(offspring)
 
         # Replace the current population by the offspring
-        # the best offsprings in R replace the pworst% individual
-        # of the population P
+        #the best offsprings in R replace the pworst% individual
+        #of the population P
         population[:] = offspring
 
         # Append the current generation statistics to the logbook
@@ -144,13 +204,9 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, neat, neatcx, h,pelit,n_cor
         logbook.record(gen=gen, nevals=len(invalid_ind), **record)
         if verbose:
             print logbook.stream
-        if neat:
-            out = open('popgen_%d_%d.txt' % (problem, n_corr), 'a')
-            for ind in population:
-                out.write('\n%s;%s;%s;%s;%s;%s;%s' % (gen, len(ind), ind.height, ind.get_specie(), ind.fitness.values, ind.get_fsharing(), ind))
-        else:
-            out = open('popgen_%d_%d.txt' % (problem, n_corr), 'a')
-            for ind in population:
-                out.write('\n%s;%s;%s;%s;%s' %(gen,len(ind), ind.height, ind.fitness.values, ind))
+
+        out=open('popgen_%d_%d.txt'%(p, n_corr),'a')
+        for ind in population:
+            out.write('\n%s;%s;%s;%s;%s;%s;%s' %(gen,len(ind), ind.height, ind.get_specie(), ind.fitness.values[0], ind.get_fsharing(), ind))
 
     return population, logbook
