4051db81ee43b0191c8600bb9eca78b7e800882b
saarahy <juarez.s.perla@gmail.com>
2016-01-21 16:17:46 -0800

    funcionando

---
+++ minpack_conf.py
@@ -0,0 +1,351 @@
+def _general_function(params, xdata, ydata, function, strg):
+    return function(strg, xdata, *params) - ydata
+
+def curve_fit_2(f,strg, xdata, ydata, p0=None, sigma=None, absolute_sigma=False,
+              check_finite=True, **kw):
+    """
+    Use non-linear least squares to fit a function, f, to data.
+
+    Assumes ``ydata = f(xdata, *params) + eps``
+
+    Parameters
+    ----------
+    f : callable
+        The model function, f(x, ...).  It must take the independent
+        variable as the first argument and the parameters to fit as
+        separate remaining arguments.
+    xdata : An M-length sequence or an (k,M)-shaped array
+        for functions with k predictors.
+        The independent variable where the data is measured.
+    ydata : M-length sequence
+        The dependent data --- nominally f(xdata, ...)
+    p0 : None, scalar, or N-length sequence, optional
+        Initial guess for the parameters.  If None, then the initial
+        values will all be 1 (if the number of parameters for the function
+        can be determined using introspection, otherwise a ValueError
+        is raised).
+    sigma : None or M-length sequence, optional
+        If not None, the uncertainties in the ydata array. These are used as
+        weights in the least-squares problem
+        i.e. minimising ``np.sum( ((f(xdata, *popt) - ydata) / sigma)**2 )``
+        If None, the uncertainties are assumed to be 1.
+    absolute_sigma : bool, optional
+        If False, `sigma` denotes relative weights of the data points.
+        The returned covariance matrix `pcov` is based on *estimated*
+        errors in the data, and is not affected by the overall
+        magnitude of the values in `sigma`. Only the relative
+        magnitudes of the `sigma` values matter.
+
+        If True, `sigma` describes one standard deviation errors of
+        the input data points. The estimated covariance in `pcov` is
+        based on these values.
+    check_finite : bool, optional
+        If True, check that the input arrays do not contain nans of infs,
+        and raise a ValueError if they do. Setting this parameter to
+        False may silently produce nonsensical results if the input arrays
+        do contain nans.
+        Default is True.
+
+    Returns
+    -------
+    popt : array
+        Optimal values for the parameters so that the sum of the squared error
+        of ``f(xdata, *popt) - ydata`` is minimized
+    pcov : 2d array
+        The estimated covariance of popt. The diagonals provide the variance
+        of the parameter estimate. To compute one standard deviation errors
+        on the parameters use ``perr = np.sqrt(np.diag(pcov))``.
+
+        How the `sigma` parameter affects the estimated covariance
+        depends on `absolute_sigma` argument, as described above.
+
+    Raises
+    ------
+    OptimizeWarning
+        if covariance of the parameters can not be estimated.
+
+    ValueError
+        if ydata and xdata contain NaNs.
+
+    See Also
+    --------
+    leastsq
+
+    Notes
+    -----
+    The algorithm uses the Levenberg-Marquardt algorithm through `leastsq`.
+    Additional keyword arguments are passed directly to that algorithm.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from scipy.optimize import curve_fit
+    >>> def func(x, a, b, c):
+    ...     return a * np.exp(-b * x) + c
+
+    >>> xdata = np.linspace(0, 4, 50)
+    >>> y = func(xdata, 2.5, 1.3, 0.5)
+    >>> ydata = y + 0.2 * np.random.normal(size=len(xdata))
+
+    >>> popt, pcov = curve_fit(func, xdata, ydata)
+
+    """
+
+    if p0 is None:
+        # determine number of parameters by inspecting the function
+        import inspect
+        args, varargs, varkw, defaults = inspect.getargspec(f)
+        if len(args) < 2:
+            msg = "Unable to determine number of fit parameters."
+            raise ValueError(msg)
+        if 'self' in args:
+            p0 = [1.0] * (len(args)-2)
+        else:
+            p0 = [1.0] * (len(args)-1)
+
+    # Check input arguments
+    if isscalar(p0):
+        p0 = array([p0])
+
+    # NaNs can not be handled
+    if check_finite:
+        ydata = np.asarray_chkfinite(ydata)
+    else:
+        ydata = np.asarray(ydata)
+    if isinstance(xdata, (list, tuple, np.ndarray)):
+        # `xdata` is passed straight to the user-defined `f`, so allow
+        # non-array_like `xdata`.
+        if check_finite:
+            xdata = np.asarray_chkfinite(xdata)
+        else:
+            xdata = np.asarray(xdata)
+
+    args = (xdata, ydata, f, strg)
+    if sigma is None:
+        func = _general_function
+    else:
+        func = _weighted_general_function
+        args += (1.0 / asarray(sigma),)
+
+    # Remove full_output from kw, otherwise we're passing it in twice.
+    return_full = kw.pop('full_output', False)
+    res = leastsq(func, p0, args=args, full_output=1, **kw)
+    (popt, pcov, infodict, errmsg, ier) = res
+
+    if ier not in [1, 2, 3, 4]:
+        return p0, p0, infodict, errmsg, ier
+        #msg = "Optimal parameters not found: " + errmsg
+        #raise RuntimeError(msg)
+
+    warn_cov = False
+    if pcov is None:
+        # indeterminate covariance
+        pcov = zeros((len(popt), len(popt)), dtype=float)
+        pcov.fill(inf)
+        warn_cov = True
+    elif not absolute_sigma:
+        if len(ydata) > len(p0):
+            s_sq = (asarray(func(popt, *args))**2).sum() / (len(ydata) - len(p0))
+            pcov = pcov * s_sq
+        else:
+            pcov.fill(inf)
+            warn_cov = True
+
+    #if warn_cov:
+        #warnings.warn('Covariance of the parameters could not be estimated',
+         #       category=OptimizeWarning)
+
+    if return_full:
+        return popt, pcov, infodict, errmsg, ier
+    else:
+        return popt, pcov
+
+def leastsq(func, x0, args=(), Dfun=None, full_output=0,
+            col_deriv=0, ftol=1.49012e-8, xtol=1.49012e-8,
+            gtol=0.0, maxfev=0, epsfcn=None, factor=100, diag=None):
+    """
+    Minimize the sum of squares of a set of equations.
+
+    ::
+
+        x = arg min(sum(func(y)**2,axis=0))
+                 y
+
+    Parameters
+    ----------
+    func : callable
+        should take at least one (possibly length N vector) argument and
+        returns M floating point numbers. It must not return NaNs or
+        fitting might fail.
+    x0 : ndarray
+        The starting estimate for the minimization.
+    args : tuple, optional
+        Any extra arguments to func are placed in this tuple.
+    Dfun : callable, optional
+        A function or method to compute the Jacobian of func with derivatives
+        across the rows. If this is None, the Jacobian will be estimated.
+    full_output : bool, optional
+        non-zero to return all optional outputs.
+    col_deriv : bool, optional
+        non-zero to specify that the Jacobian function computes derivatives
+        down the columns (faster, because there is no transpose operation).
+    ftol : float, optional
+        Relative error desired in the sum of squares.
+    xtol : float, optional
+        Relative error desired in the approximate solution.
+    gtol : float, optional
+        Orthogonality desired between the function vector and the columns of
+        the Jacobian.
+    maxfev : int, optional
+        The maximum number of calls to the function. If `Dfun` is provided
+        then the default `maxfev` is 100*(N+1) where N is the number of elements
+        in x0, otherwise the default `maxfev` is 200*(N+1).
+    epsfcn : float, optional
+        A variable used in determining a suitable step length for the forward-
+        difference approximation of the Jacobian (for Dfun=None).
+        Normally the actual step length will be sqrt(epsfcn)*x
+        If epsfcn is less than the machine precision, it is assumed that the
+        relative errors are of the order of the machine precision.
+    factor : float, optional
+        A parameter determining the initial step bound
+        (``factor * || diag * x||``). Should be in interval ``(0.1, 100)``.
+    diag : sequence, optional
+        N positive entries that serve as a scale factors for the variables.
+
+    Returns
+    -------
+    x : ndarray
+        The solution (or the result of the last iteration for an unsuccessful
+        call).
+    cov_x : ndarray
+        Uses the fjac and ipvt optional outputs to construct an
+        estimate of the jacobian around the solution. None if a
+        singular matrix encountered (indicates very flat curvature in
+        some direction).  This matrix must be multiplied by the
+        residual variance to get the covariance of the
+        parameter estimates -- see curve_fit.
+    infodict : dict
+        a dictionary of optional outputs with the key s:
+
+        ``nfev``
+            The number of function calls
+        ``fvec``
+            The function evaluated at the output
+        ``fjac``
+            A permutation of the R matrix of a QR
+            factorization of the final approximate
+            Jacobian matrix, stored column wise.
+            Together with ipvt, the covariance of the
+            estimate can be approximated.
+        ``ipvt``
+            An integer array of length N which defines
+            a permutation matrix, p, such that
+            fjac*p = q*r, where r is upper triangular
+            with diagonal elements of nonincreasing
+            magnitude. Column j of p is column ipvt(j)
+            of the identity matrix.
+        ``qtf``
+            The vector (transpose(q) * fvec).
+
+    mesg : str
+        A string message giving information about the cause of failure.
+    ier : int
+        An integer flag.  If it is equal to 1, 2, 3 or 4, the solution was
+        found.  Otherwise, the solution was not found. In either case, the
+        optional output variable 'mesg' gives more information.
+
+    Notes
+    -----
+    "leastsq" is a wrapper around MINPACK's lmdif and lmder algorithms.
+
+    cov_x is a Jacobian approximation to the Hessian of the least squares
+    objective function.
+    This approximation assumes that the objective function is based on the
+    difference between some observed target data (ydata) and a (non-linear)
+    function of the parameters `f(xdata, params)` ::
+
+           func(params) = ydata - f(xdata, params)
+
+    so that the objective function is ::
+
+           min   sum((ydata - f(xdata, params))**2, axis=0)
+         params
+
+    """
+    x0 = asarray(x0).flatten()
+    n = len(x0)
+    if not isinstance(args, tuple):
+        args = (args,)
+    shape, dtype = _check_func('leastsq', 'func', func, x0, args, n)
+    m = shape[0]
+    # if n > m:
+    #     raise TypeError('Improper input: N=%s must not exceed M=%s' % (n, m))
+    if epsfcn is None:
+        epsfcn = finfo(dtype).eps
+    if Dfun is None:
+        if maxfev == 0:
+            maxfev = 200*(n + 1)
+        retval = _minpack._lmdif(func, x0, args, full_output, ftol, xtol,
+                                 gtol, maxfev, epsfcn, factor, diag)
+    else:
+        if col_deriv:
+            _check_func('leastsq', 'Dfun', Dfun, x0, args, n, (n, m))
+        else:
+            _check_func('leastsq', 'Dfun', Dfun, x0, args, n, (m, n))
+        if maxfev == 0:
+            maxfev = 100 * (n + 1)
+        retval = _minpack._lmder(func, Dfun, x0, args, full_output, col_deriv,
+                                 ftol, xtol, gtol, maxfev, factor, diag)
+
+    errors = {0: ["Improper input parameters.", TypeError],
+              1: ["Both actual and predicted relative reductions "
+                  "in the sum of squares\n  are at most %f" % ftol, None],
+              2: ["The relative error between two consecutive "
+                  "iterates is at most %f" % xtol, None],
+              3: ["Both actual and predicted relative reductions in "
+                  "the sum of squares\n  are at most %f and the "
+                  "relative error between two consecutive "
+                  "iterates is at \n  most %f" % (ftol, xtol), None],
+              4: ["The cosine of the angle between func(x) and any "
+                  "column of the\n  Jacobian is at most %f in "
+                  "absolute value" % gtol, None],
+              5: ["Number of calls to function has reached "
+                  "maxfev = %d." % maxfev, ValueError],
+              6: ["ftol=%f is too small, no further reduction "
+                  "in the sum of squares\n  is possible.""" % ftol,
+                  ValueError],
+              7: ["xtol=%f is too small, no further improvement in "
+                  "the approximate\n  solution is possible." % xtol,
+                  ValueError],
+              8: ["gtol=%f is too small, func(x) is orthogonal to the "
+                  "columns of\n  the Jacobian to machine "
+                  "precision." % gtol, ValueError],
+              'unknown': ["Unknown error.", TypeError]}
+
+    info = retval[-1]    # The FORTRAN return value
+
+    if info not in [1, 2, 3, 4] and not full_output:
+        if info in [5, 6, 7, 8]:
+            warnings.warn(errors[info][0], RuntimeWarning)
+        else:
+            try:
+                raise errors[info][1](errors[info][0])
+            except KeyError:
+                raise errors['unknown'][1](errors['unknown'][0])
+
+    mesg = errors[info][0]
+    if full_output:
+        cov_x = None
+        if info in [1, 2, 3, 4]:
+            from numpy.dual import inv
+            from numpy.linalg import LinAlgError
+            perm = take(eye(n), retval[1]['ipvt'] - 1, 0)
+            r = triu(transpose(retval[1]['fjac'])[:n, :])
+            R = dot(r, perm)
+            try:
+                cov_x = inv(dot(transpose(R), R))
+            except (LinAlgError, ValueError):
+                pass
+        return (retval[0], cov_x) + retval[1:-1] + (mesg, info)
+    else:
+        return (retval[0], info)
