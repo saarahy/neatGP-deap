38c7589b37528422b14883ae26e072f55cc43677
saarahy <juarez.s.perla@gmail.com>
2016-04-04 13:03:39 -0700

    cruce & mutacion

--- eaneatGP.py
+++ eaneatGP.py
@@ -1,10 +1,51 @@
 import random
+import copy
 from deap import tools
 from neat_operators import neatGP
 from speciation import ind_specie, species, specie_parents_child
 from fitness_sharing import SpeciesPunishment
 from ParentSelection import p_selection
 from my_operators import avg_nodes
+
+
+def varOr(population, toolbox, cxpb, mutpb):
+    assert (cxpb + mutpb) <= 1.0, ("The sum of the crossover and mutation "
+        "probabilities must be smaller or equal to 1.0.")
+
+    new_pop = [toolbox.clone(ind) for ind in population]
+    offspring = []
+    for i in range(1, len(new_pop), 2):
+        new_pop[i-1].off_cx_set(0), new_pop[i].off_cx_set(0)
+        if random.random() < cxpb and len(ind)>1:
+            new_pop[i-1].off_cx_set(1)
+            new_pop[i].off_cx_set(1)
+            offspring1, offspring2 = toolbox.mate(new_pop[i-1], new_pop[i])
+            del offspring1.fitness.values
+            del offspring2.fitness.values
+            offspring1.bestspecie_set(0), offspring2.bestspecie_set(0)
+            offspring1.LS_applied_set(0), offspring2.LS_applied_set(0)
+            offspring1.LS_fitness_set(None), offspring2.LS_fitness_set(None)
+            offspring1.off_cx_set(1), offspring2.off_cx_set(1)
+            offspring.append(offspring1)
+            offspring.append(offspring2)
+    for i in range(len(new_pop)):
+        if new_pop[i].off_cx_get() != 1:
+            if random.random() < (cxpb+mutpb):  # Apply mutation
+                offspring1, = toolbox.mutate(new_pop[i])
+                del offspring1.fitness.values
+                offspring1.bestspecie_set(0)
+                offspring1.LS_applied_set(0)
+                offspring1.LS_fitness_set(None)
+                offspring1.off_mut_set(1)
+                offspring.append(offspring1)
+
+    if len(offspring) < len(population):
+        for i in range(len(new_pop)):
+            if new_pop[i].off_mut_get() != 1 and new_pop[i].off_cx_get() != 1:
+                offspring1 = copy.deepcopy(new_pop[i])
+                offspring.append(offspring1)
+
+    return offspring
 
 
 def varAnd(population, toolbox, cxpb, mutpb):
@@ -174,7 +215,7 @@ def neat_GP( population, toolbox, cxpb, mutpb, ngen, neat_alg, neat_cx, neat_h, 
             cx = 1
             offspring = neatGP(toolbox, parents, cxpb, mutpb, n, mut, cx, neat_pelit)
         else:
-            offspring = varAnd(parents, toolbox, cxpb, mutpb)
+            offspring = varOr(parents, toolbox, cxpb, mutpb)
 
         if neat_alg:  # Assign species
             specie_parents_child(parents, offspring, neat_h)
